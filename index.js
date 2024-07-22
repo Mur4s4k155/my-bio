ca6990673d34ad77bcbb186280f07b75bf1de835
Mur4s4k155 <gustavohhsardinha@gmail.com>
2024-07-22 16:56:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstName = "Guzttavo";
+let lastName = "Henrique";
+let maritalStatus = "solteiro forever";
+let country = "Brasil";
+let age = 17;
+ 
+console.log(`
+Meu nome é ${firstName} ${lastName}. Minha idade é ${age}. Meu estado civil é ${maritalStatus}. Eu moro no ${country}
+`);
+
+
